219e34831ce7f2f9cda76beb975f162781df4752
Sonia <coleysonia@gmail.com>
2023-05-02 21:13:28 -0400

    p

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sonia\OneDrive\Desktop\Documents\ecomproj\ecomm\venv
+set VIRTUAL_ENV=C:\Users\sonia\OneDrive\Desktop\Documents\ecomproj\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
